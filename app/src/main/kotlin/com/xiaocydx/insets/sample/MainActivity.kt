84282b731accfca1e3b4609c5ba4bd080bd6c3a5
Xcc <xiaocydx@163.com>
2023-12-26 22:22:53 +0800

    first commit

---
+++ app/src/main/kotlin/com/xiaocydx/insets/sample/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.xiaocydx.insets.sample
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * @author xcc
+ * @date 2023/12/26
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
