5e19eb339212e106a113863359aaae7083fb28eb
xcc <xiaocydx@163.com>
2026-02-08 16:17:40 +0800

    perf: 调整示例代码顺序

--- app/src/main/kotlin/com/xiaocydx/insets/sample/MainActivity.kt
+++ app/src/main/kotlin/com/xiaocydx/insets/sample/MainActivity.kt
@@ -31,12 +31,6 @@ class MainActivity : AppCompatActivity(), SystemBar {
             "Insetter" desc "提供常用的WindowInsets扩展属性和扩展函数" start InsetterActivity::class
         ),
 
-        "insets-compat".elements(
-            "ImeAnimationCompat" desc "修改Android 11及以上IME动画的时长和插值器" start ImeAnimationCompatActivity::class,
-            "FullscreenCompat" desc "Android 11以下FLAG_FULLSCREEN的兼容方案" start FullscreenCompatActivity::class,
-            "ImmutableCompat" desc "Android 9.0以下的WindowInsets可变的兼容方案" start ImmutableCompatActivity::class
-        ),
-
         "insets-systembar".elements(
             "Basic" desc "SystemBar的基本使用" start SystemBarBasicActivity::class,
             "Restore" desc "SystemBar恢复window属性" start SystemBarRestoreActivity::class,
@@ -57,6 +51,12 @@ class MainActivity : AppCompatActivity(), SystemBar {
                 ${LintSample::class.java.simpleName}.kt 演示了检查场景和IDE提示。
                 检查报告 app\build\reports\lint-results-debug.html 可放入浏览器查看。
                 """.trimIndent()
+        ),
+
+        "insets-compat".elements(
+            "ImeAnimationCompat" desc "修改Android 11及以上IME动画的时长和插值器" start ImeAnimationCompatActivity::class,
+            "FullscreenCompat" desc "Android 11以下FLAG_FULLSCREEN的兼容方案" start FullscreenCompatActivity::class,
+            "ImmutableCompat" desc "Android 9.0以下的WindowInsets可变的兼容方案" start ImmutableCompatActivity::class
         )
     )
 }
